4344b2b1bc7fe52b4a42e77ef1782bfd5fe814e8
barbaracalderon <bcalderoni.ti@gmail.com>
2024-02-07 10:00:21 -0300

    refactor: add 'barcode' to filename

--- barcodes_api_dev/drivers/one_three_eight_handler.py
+++ barcodes_api_dev/drivers/one_three_eight_handler.py
@@ -5,6 +5,6 @@ from barcode.writer import ImageWriter
 class OneThreeEightHandler:
     def create_one_three_eight_barcode(self, product_name: str) -> str:
         tag = Code128(code=product_name, writer=ImageWriter())
-        tag_name = "one_three_eight"
+        tag_name = "barcode_138"
         tag.save(tag_name)
         return tag_name
--- barcodes_api_dev/drivers/upc_handler.py
+++ barcodes_api_dev/drivers/upc_handler.py
@@ -5,6 +5,6 @@ from barcode.writer import ImageWriter
 class UpcHandler:
     def create_upc_barcode(self, product_number: str) -> str:
         tag = UniversalProductCodeA(upc=product_number, writer=ImageWriter())
-        tag_name = "upc"
+        tag_name = "barcode_upc"
         tag.save(tag_name)
         return tag_name
