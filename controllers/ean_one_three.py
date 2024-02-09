8fd7bd94196669a31bdc3574af29ae3ad298d2b3
barbaracalderon <bcalderoni.ti@gmail.com>
2024-02-09 15:22:04 -0300

    feat: ean-13 controller calls buffer image handler

--- controllers/ean_one_three.py
+++ controllers/ean_one_three.py
@@ -1,5 +1,5 @@
 from typing import Dict
-from models.drivers import EanOneThreeHandler, ResponseFormatHandler
+from models.drivers import EanOneThreeHandler, BufferedImageHandler, ResponseFormatHandler
 from models import EanOneThreeModel
 from flask import jsonify
 
@@ -7,15 +7,25 @@ from flask import jsonify
 class EanOneThreeController:
 
     def create(self, product_number: str) -> Dict:
-        tag_path = self.__create_ean_one_three_tag(product_number)
-        response_format_handler = ResponseFormatHandler()
-        formatted_response = response_format_handler.format_response(tag_path)
+        barcode_tag = self.__create_ean_one_three_tag(product_number)
+        buffered_image = self.__create_buffer_image(barcode_tag)
+        formatted_response = self.__create_formatted_response(buffered_image)
         return formatted_response
 
     def __create_ean_one_three_tag(self, product_number: str) -> str:
         ean_one_three_handler = EanOneThreeHandler()
         tag_name = ean_one_three_handler.create_ean_one_three_barcode(product_number)
         return tag_name
+    
+    def __create_buffer_image(self, barcode_tag):
+        buffered_image_handler = BufferedImageHandler()
+        buffered_image = buffered_image_handler.create_buffer_image(barcode_tag)
+        return buffered_image
+
+    def __create_formatted_response(self, buffered_image):
+        response_format_handler = ResponseFormatHandler()
+        formatted_response = response_format_handler.format_response(buffered_image)
+        return formatted_response
 
     def get_data(self) -> Dict:
         ean_one_three_model = EanOneThreeModel()
--- models/drivers/ean_one_three_handler.py
+++ models/drivers/ean_one_three_handler.py
@@ -6,6 +6,4 @@ class EanOneThreeHandler:
 
     def create_ean_one_three_barcode(self, product_number: str) -> str:
         tag = EAN13(ean=product_number, writer=ImageWriter())
-        tag_name = "barcode_ean_13"
-        tag.save(tag_name)
-        return tag_name
+        return tag
